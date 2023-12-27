d90feb5e474bf8b36d1f4f3d1700739f61a50508
Mats Dodd <mats.dodd12@gmail.com>
2023-12-27 13:54:48 +0100

    added interactive reg output-static

---
+++ src/output/output.py
@@ -0,0 +1,19 @@
+from IPython.display import display, HTML
+import pandas as pd
+import numpy as np
+
+
+def generate_summary():
+
+    html_content = HTML("""
+    <h1>Hello, World!</h1>
+    <p>This is a paragraph in HTML.</p>
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
+    """)
+
+    return html_content
+
